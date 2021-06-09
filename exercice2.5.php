95315e4471e48abdf296f7f6aa9e6ce734927b33
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 15:17:04 +0200

    correction ajout HTML

--- exercice2.5.php
+++ exercice2.5.php
@@ -1,11 +1,25 @@
-<?php
-$gender = "Femme";
-if ($gender == "Homme")
-{
-    echo "C'est un développeur !!!";
-}
-else
-{
-    echo "C'est une développeuse !!!";
-}
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2.5</title>
+</head>
+<body>
+    <h1>Exercice 5 Partie 2 : Les conditions PHP</h1>
+    <p>
+        <?php
+        $gender = "Femme";
+        if ($gender == "Homme")
+        {
+            echo "C'est un développeur !!!";
+        }
+        else
+        {
+            echo "C'est une développeuse !!!";
+        }
+        ?>
+    </p>
+</body>
+</html>
